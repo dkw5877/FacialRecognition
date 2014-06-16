354947e64f4ec7b6aaef36a98e72336818d2fa93
dkw5877 <dkw5877@gmail.com>
2014-06-15 18:43:07 -0700

    add methods for finding facial features in image

--- FacialRecognition/PhotoViewController.m
+++ FacialRecognition/PhotoViewController.m
@@ -18,7 +18,8 @@
 
 @implementation PhotoViewController
             
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     
     [super viewDidLoad];
     self.view.backgroundColor = [UIColor blueColor];
@@ -37,26 +38,31 @@
     _imageView.image =  _photos.firstObject;
     
     [self applyFilter:@"CISepiaTone" toImage:_photos.firstObject withContextOptions:nil];
+    [self findFaces:_photos.firstObject];
     
 }
 
 
 - (void)applyFilter:(NSString*)imageFilter toImage:(UIImage*)image withContextOptions:(NSDictionary*)options
 {
-    //render the image of the GPU
+    //render the image on the GPU
     options = @{@"kCIContextUseSoftwareRenderer":@"YES"};
+    
+    //create a context for the image. This can be CPU or GPU depending upon performance requirement such as real-time processing
     CIContext* context = [CIContext contextWithOptions:options];
     
+    //convert the UIImage to NSData for creating a CIImage
     NSData* data = UIImageJPEGRepresentation(image, 1.0);
     CIImage* aImage = [CIImage imageWithData:data];
     
-    //create the image filter
+    //create the image filter and set values
     CIFilter* filter = [CIFilter filterWithName:imageFilter];
     [filter setValue:aImage forKey:kCIInputImageKey];
     
-    
+    //get the resulting image of the applied filter
     CIImage* result = [filter valueForKey:kCIOutputImageKey];
     
+    //get the extent
     CGRect extent = [result extent];
     
     //render the image to a Core Graphics image that is ready for display or saving to a file
@@ -64,6 +70,60 @@
     
     _imageView.image = [UIImage imageWithCGImage:imageRef];
     
+}
+
+- (NSArray*)findFaces:(UIImage*)image
+{
+    //convert the UIImage to NSData for creating a CIImage
+    //Note: the face detector will only work in CIImage types
+    NSData* data = UIImageJPEGRepresentation(image, 1.0);
+    CIImage* aImage = [CIImage imageWithData:data];
+    
+    
+    //create the context for image processing
+    CIContext *context = [CIContext contextWithOptions:nil];
+    
+    //set the options
+    NSDictionary *opts = @{ CIDetectorAccuracy : CIDetectorAccuracyHigh };
+    
+    //create a detector for human faces, the only type of detector you can create is one for human faces
+    CIDetector *detector = [CIDetector detectorOfType:CIDetectorTypeFace
+                                              context:context
+                                              options:opts];
+
+    //in order to find the faces, we need to tell the detector the orientation of the image, this can be obtained from the image's properties using the key Orientation
+    opts = @{ CIDetectorImageOrientation :
+                  [[aImage properties] valueForKey:@"Orientation"] };
+    
+    //Use the detector to find the features (i.e. faces in the image). There will be an entry for each face.
+    NSArray *features = [detector featuresInImage:aImage options:opts];
+    return features;
+}
+
+
+- (void)getFeaturesFromFace:(NSArray*)features
+{
+    //features is an array of type CIFeature return from the detector object
+    
+    for (CIFaceFeature *f in features)
+    {
+        //NSLog(NSStringFromRect(f.bounds));
+        
+        if (f.hasLeftEyePosition)
+        {
+            NSLog(@"Left eye %g %g", f.leftEyePosition.x, f.leftEyePosition.y);
+        }
+        
+        if (f.hasRightEyePosition)
+        {
+            NSLog(@"Right eye %g %g", f.rightEyePosition.x, f.rightEyePosition.y);
+        }
+        
+        if (f.hasMouthPosition)
+        {
+            NSLog(@"Mouth %g %g", f.mouthPosition.x, f.mouthPosition.y);
+        }
+    }
 }
 
 @end
